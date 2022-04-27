df5b1cde4e5c836f206f003749057ad4292a5e39
Your Name <thangqtran23@gmail.com>
2022-04-27 23:11:13 +0700

    fix update feature

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,6 @@
 const Campground = require('../models/campground');
+const { cloudinary } = require("../cloudinary");
+
 
 
 module.exports.index = async (req,res) => {
@@ -37,6 +39,17 @@ module.exports.updateCampground = async (req,res,next) => {
     const { id } = req.params;
     //them authorize 
     const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
+    //xu li xoa hinh de update 
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    campground.images.push(...imgs);
+    await campground.save();
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+    }    
+    //
     req.flash('success','Successfully updated a new campgrounds');
     res.redirect(`/campgrounds/${campground._id}`)
 }
